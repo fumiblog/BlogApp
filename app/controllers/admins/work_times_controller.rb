00f101634c2efc3b6f7a72f247d9fa6315be7b9f
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-03-01 09:06:54 +0900

    [stopwatch start] 3-17

--- app/controllers/admins/work_times_controller.rb
+++ app/controllers/admins/work_times_controller.rb
@@ -1,2 +1,29 @@
 class Admins::WorkTimesController < ApplicationController
+  def index
+    @work_times = WorkTime.all
+    @work_time = WorkTime.new
+  end
+
+  def create
+    @work_time = WorkTime.new(work_time_params)
+    @work_time.save
+    redirect_to admins_work_times_path
+  end
+
+  def destroy
+    @work_time = WorkTime(params[:id])
+    @work_time.destroy
+    redirect_to admins_work_times_path
+  end
+
+  private
+  def work_time_params
+    params.require(:work_time).permit(
+      :name,
+      :worktype_id,
+      :start,
+      :goal
+    )
+  end
+
 end
