28081c2628a13f70bf93a6e27b844c4cb5e5df6e
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-03-22 14:30:04 +0900

    [chart gon] 3-22

--- app/controllers/admins/dashboards_controller.rb
+++ app/controllers/admins/dashboards_controller.rb
@@ -4,8 +4,8 @@ class Admins::DashboardsController < ApplicationController
     @data = Footprint.where(genre_id: 1)
     @datas = @data.select(:date, :price)
     @times = [date:2021-01-28]
-    # @genre_json = Genre.pluck(:name).to_json
-    @genre_json = Genre.pluck(:name)
+    gon.genre_json = Genre.pluck(:name)
+    gon.footprint_json = Footprint.group(:genre_id).sum(:price).values
     # @footprint_json = Footprint.group(:genre_id).sum(:price).to_json
     # @footprint_json = Footprint.group(:genre_id).sum(:price).pluck(:price).to_json
   end
