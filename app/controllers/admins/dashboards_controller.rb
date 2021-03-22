0b68e9319f5288e5127c5e143ca6d5c986437cfc
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-03-22 12:11:00 +0900

    [chart.js second] 3-21

--- app/controllers/admins/dashboards_controller.rb
+++ app/controllers/admins/dashboards_controller.rb
@@ -4,8 +4,10 @@ class Admins::DashboardsController < ApplicationController
     @data = Footprint.where(genre_id: 1)
     @datas = @data.select(:date, :price)
     @times = [date:2021-01-28]
-    @genre_json = Genre.pluck(:name).to_json
-    @footprint_json = Footprint.group(:genre_id).pluck(:price).sum(:price).to_json
+    # @genre_json = Genre.pluck(:name).to_json
+    @genre_json = Genre.pluck(:name)
+    # @footprint_json = Footprint.group(:genre_id).sum(:price).to_json
+    # @footprint_json = Footprint.group(:genre_id).sum(:price).pluck(:price).to_json
   end
 
   def create
