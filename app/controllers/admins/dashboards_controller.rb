317f9b8a3c40b96506b2348d2a8f521ea79e1876
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-03-27 14:33:57 +0900

    [chart.js blog_count] 3-22

--- app/controllers/admins/dashboards_controller.rb
+++ app/controllers/admins/dashboards_controller.rb
@@ -6,8 +6,8 @@ class Admins::DashboardsController < ApplicationController
     @times = [date:2021-01-28]
     gon.genre_json = Genre.pluck(:name)
     gon.footprint_json = Footprint.group(:genre_id).sum(:price).values
-    # @footprint_json = Footprint.group(:genre_id).sum(:price).to_json
-    # @footprint_json = Footprint.group(:genre_id).sum(:price).pluck(:price).to_json
+    gon.blog_writingdate = Blog.group("date(created_at)").pluck("date(created_at)")
+    gon.blog_writingcount = Blog.group("date(created_at)").count.values
   end
 
   def create
