0c54550e9e8b11adf3c07c9e9abdae057189e9be
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-03-28 21:37:08 +0900

    [chart.js blogcountmonth 1] 3-23

--- app/controllers/admins/dashboards_controller.rb
+++ app/controllers/admins/dashboards_controller.rb
@@ -8,6 +8,10 @@ class Admins::DashboardsController < ApplicationController
     gon.footprint_json = Footprint.group(:genre_id).sum(:price).values
     gon.blog_writingdate = Blog.group("date(created_at)").pluck("date(created_at)")
     gon.blog_writingcount = Blog.group("date(created_at)").count.values
+    gon.blog_writingmonth = Blog.all.group_by{ |record| record.created_at.strftime("%Y-%m") }.keys
+    @month_record = Blog.all.group_by{ |record| record.created_at.strftime("%Y-%m") }.values
+    gon.blog_writingmonthcount = @month_record.count
+    byebug
   end
 
   def create
